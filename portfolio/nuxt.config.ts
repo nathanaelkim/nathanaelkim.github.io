f867dd397f01bd93f106174a95c11d12e512c5b6
nathanaelkim <nathanael.gom@gmail.com>
2023-12-12 15:57:19 +0100

    get config

--- portfolio/nuxt.config.ts
+++ portfolio/nuxt.config.ts
@@ -1,5 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content']
+  modules: ['@nuxt/content'],
+  app: {
+    head: {
+      meta: [
+        {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+        {content: "Portfolio von Nathanael Kim"},
+        {
+          hid: "og:url",
+          property: "og:url",
+          content: "nathanaelkim.github.io",
+        },
+        {
+          hid: "og:title",
+          property: "og:title",
+          content: "Portfolio - Nathanael Kim",
+        },
+      ],
+      title: 'Portfolio - Nathanael Kim',
+    },
+  },
 })
