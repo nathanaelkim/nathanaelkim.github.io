86726389dc0bd9eb130eb6b4f8bcfe8c1e090d0d
nathanaelkim <nathanael.gom@gmail.com>
2023-12-12 16:03:38 +0100

    enter content by the home site

--- portfolio/pages/[...slug].vue
+++ portfolio/pages/[...slug].vue
@@ -1,5 +1,13 @@
 <template>
   <main>
+    <header>
+      <h3 class="welcome">Herzlich Willkommen!</h3>
+      <h3 class="typewrite" data-period="2000" type='["Ich bin Schüler.", "Ich bin Software Entwickler.", "Ich bin Designer.", "Ich bin Gründer."]'></h3>
+      <span class="wrap"></span>
+    </header>
     <ContentDoc />
+    <footer>
+      <p>&copy;2023 Nathanael Kim</p>
+    </footer>
   </main>
 </template>
