3687e95dea4fec479be70960dba4b1937c7d6897
nathanaelkim <nathanael.gom@gmail.com>
2024-10-22 10:52:32 +0200

    seperate map in a component

---
+++ cdn/sustainathy-postcode-map/src/app/map/map.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map',
+  standalone: true,
+  templateUrl: './map.component.svg',
+  styleUrl: './map.component.scss'
+})
+export class MapComponent {
+
+}
