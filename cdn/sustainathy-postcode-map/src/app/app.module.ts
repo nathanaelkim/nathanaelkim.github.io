3555664d9ff2862281ce4003e1be6bb94942250b
nathanaelkim <nathanael.gom@gmail.com>
2024-11-14 13:27:17 +0100

    style component

---
+++ cdn/sustainathy-postcode-map/src/app/app.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { MapComponent } from "./map/map.component";
+import { AppComponent } from "./app.component";
+
+@NgModule({
+    declarations: {
+        AppComponent,
+        MapComponent
+    },
+})
